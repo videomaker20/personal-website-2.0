f96bfd4ab8ecf79fc6589ca19d9d6d749c14271c
Okky <prime.okky@gmail.com>
2025-10-31 07:32:00 +0700

    minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
     basePath: '/personal-website-2.0',
+    assetPrefix: '/personal-website-2.0',
     trailingSlash: true,
 };
 
