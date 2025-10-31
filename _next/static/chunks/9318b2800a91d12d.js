405685e7c0ec90923137aab4e697ee42634b4e32
Okky <prime.okky@gmail.com>
2025-10-31 07:09:30 +0700

    deploy site

---
+++ _next/static/chunks/9318b2800a91d12d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/8353f8a349023ce1.js",
+  "static/chunks/turbopack-25be5d3141d7f217.js"
+])
---
+++ _next/static/chunks/ea24d0a4615734b6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/8353f8a349023ce1.js",
+  "static/chunks/turbopack-ba1be557879af58b.js"
+])
